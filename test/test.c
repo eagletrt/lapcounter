3ec4d94369e3d2045cd88034e0362bca1a11871d
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2022-01-27 11:28:10 +0100

    let's try pipes

--- test/test.c
+++ test/test.c
@@ -16,12 +16,6 @@ typedef struct __is_packed {
     float longitude;
 } Primary_GPS_COORDS;
 
-size_t serialize_Primary_GPS_COORDS(uint8_t* buffer, float latitude, float longitude) {
-    Primary_GPS_COORDS primary_gps_coords = { latitude, longitude };
-    memcpy(buffer, &primary_gps_coords, sizeof(Primary_GPS_COORDS));
-    return sizeof(Primary_GPS_COORDS);
-}
-
 size_t deserialize_Primary_GPS_COORDS(uint8_t* buffer, Primary_GPS_COORDS* primary_gps_coords) {
     memcpy(primary_gps_coords, buffer, sizeof(Primary_GPS_COORDS));
     return sizeof(Primary_GPS_COORDS);
@@ -89,26 +83,8 @@ void main(int argc, char **argv) {
     lc_point_t point;   // to pass to evaluation
     lc_counter_t * lp = lc_init(NULL);  // initialization with default settings
 
-    while (1) {
-        struct can_frame frameRead;
-        nBytesRead = read(s, &frameRead, sizeof(struct can_frame));
-        uint8_t* buffer_primary_gps_coords = (uint8_t*)malloc(sizeof(Primary_GPS_COORDS));
-        buffer_primary_gps_coords[0] = (uint8_t)frameRead.data[0];
-        buffer_primary_gps_coords[1] = (uint8_t)frameRead.data[1];
-        buffer_primary_gps_coords[2] = (uint8_t)frameRead.data[2];
-        buffer_primary_gps_coords[3] = (uint8_t)frameRead.data[3];
-        buffer_primary_gps_coords[4] = (uint8_t)frameRead.data[4];
-        buffer_primary_gps_coords[5] = (uint8_t)frameRead.data[5];
-        buffer_primary_gps_coords[6] = (uint8_t)frameRead.data[6];
-        buffer_primary_gps_coords[7] = (uint8_t)frameRead.data[7];
-
-        Primary_GPS_COORDS* primary_gps_coords_d = (Primary_GPS_COORDS*)malloc(sizeof(Primary_GPS_COORDS));
-        deserialize_Primary_GPS_COORDS(buffer_primary_gps_coords, primary_gps_coords_d);
-        //printf("%f %f\n", primary_gps_coords_d->latitude, primary_gps_coords_d->longitude);
-        point.x = (double)primary_gps_coords_d->latitude;
-        point.y = (double)primary_gps_coords_d->longitude;
-        //printf("%lf, %lf\n", point.x, point.y);
-        //printf("%lf, %lf\n", point.x, point.y);
+    while (scanf("%lf %lf", &point.x, &point.y)) {
+        printf("Reading: %lf, %lf", point.x, point.y);
         if (lc_eval_point(lp, &point)) {
             struct can_frame frameWrite;
             frameWrite.can_id  = 0x6A0;  //change this
